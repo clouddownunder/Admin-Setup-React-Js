c09fa048dfc1f092b5858b4ba44871240a3bcf16
madhavitrilokninfotech <madhavi.mistry@trilokninfotech.com>
2026-06-12 12:16:22 +0530

    remove setup github cmds

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import checker from 'vite-plugin-checker';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
-  base: '/Admin-Setup-React-Js/',
-
   plugins: [
     react(),
     checker({
@@ -16,7 +14,6 @@ export default defineConfig({
       },
     }),
   ],
-
   resolve: {
     alias: [
       {
@@ -29,11 +26,9 @@ export default defineConfig({
       },
     ],
   },
-
   server: {
     port: 3030,
   },
-
   preview: {
     port: 3030,
   },
